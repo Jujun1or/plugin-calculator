ce63352c0a3601691038a30a593ac8160f30336d
Jujun1or <rmedvedkov2005@gmail.com>
2025-10-31 03:28:05 +0300

    feat(core): updated main, evaluation in while true
    - before each user input iteration, the program now recompiles and added new plugins from ./plugins/plugins_source.
    - added 'exit'/'quit' commands for termination

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,35 +1,55 @@
 #include <iostream>
+#include <filesystem>
+#include <cstdlib>
 #include "../parser/Parser.hpp"
 #include "../eval/Evaluator.hpp"
 #include "../plugin_manager/PluginManager.hpp"
 
+
 int main() {
     PluginManager plugins;
-    plugins.loadPlugins("./plugins");
+
+    plugins.buildPluginsFromSource("plugins/plugins_source", "plugins");
+    plugins.loadPlugins("plugins");
     plugins.printLoaded();
 
-    std::cout << "Enter expression: ";
-    std::string expr;
-    std::getline(std::cin, expr);
+    Evaluator eval(plugins);
+    std::cout << "\nType 'exit' to quit.\n";
 
-    Parser parser(expr);
-    if (!parser.parse()) {
-        std::cerr << "Parse error.\n";
-        return 1;
+    while (true) {
+        plugins.buildPluginsFromSource("plugins/plugins_source", "plugins");
+
+        plugins.loadPlugins("plugins");
+
+        std::cout << "\nEnter expression: ";
+        std::string expr;
+        std::getline(std::cin, expr);
+
+        if (expr == "exit" || expr == "quit")
+            break;
+
+        if (expr.empty())
+            continue;
+
+        Parser parser(expr, plugins);
+        if (!parser.parse()) {
+            std::cerr << "Parse error.\n";
+            continue;
+        }
+
+        parser.printRpn();
+
+        bool ok = false;
+        std::string err;
+        double result = eval.evaluate(parser.getRpn(), ok, err);
+
+        if (!ok) {
+            std::cerr << "Error: " << err << "\n";
+        } else {
+            std::cout << "Result: " << result << "\n";
+        }
     }
 
-    parser.printRpn();
-
-    bool ok = false;
-    std::string err;
-    Evaluator eval;
-    double result = eval.evaluate(parser.getRpn(), ok, err);
-
-    if (!ok) {
-        std::cerr << "Error: " << err << "\n";
-        return 1;
-    }
-
-    std::cout << "Result: " << result << "\n";
+    std::cout << "\nGoodbye!\n";
     return 0;
 }
